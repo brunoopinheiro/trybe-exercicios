49218287b6bda4ddf838592725f753b01433376c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-07-20 10:33:19 -0300

    exemplo aula 2

---
+++ m1-fundamentos/b4-javascript-logica_programacao/aula2/scripts.js
@@ -0,0 +1,19 @@
+// let movie = "Avengers"; //string literal
+// let score = 10.42; //number literal
+// let isValid = true; //boolean
+// let name; //undefined
+// let color = null; //redefinicao
+//variaveis com tipagem dinamica
+//possivel fazer operacoes aritmeticas (+, -, *, /, **)
+
+let salary = 3500;
+
+// console.log(salary + salary);
+// console.log(salary - salary);
+// console.log(salary * salary);
+// console.log(salary / salary);
+// console.log(3 ** 3); //potencia
+
+salary++;
+
+console.log(salary);
