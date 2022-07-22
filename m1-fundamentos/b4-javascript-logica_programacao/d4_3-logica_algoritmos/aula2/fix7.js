472a1f7df6b940920e09a0d582cd843d48051632
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-07-22 09:51:22 -0300

    finalizando conteudo teorico

---
+++ m1-fundamentos/b4-javascript-logica_programacao/d4_3-logica_algoritmos/aula2/fix7.js
@@ -0,0 +1,17 @@
+// const n = 100;
+// let sum = 0;
+
+// for (let i = 1; i <= n; i += 1){
+//     sum += i;
+// }
+
+// console.log(sum);
+
+let a = 100;
+let contador;
+for (let i = 1; a >= 5; i += 1){
+    a -= 5;
+    contador = i;
+}
+console.log(a);
+console.log(contador);
