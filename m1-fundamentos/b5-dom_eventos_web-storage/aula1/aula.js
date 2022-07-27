8a4a4bde3b5e8591796da5e6000e50df96e812cf
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-07-27 11:55:59 -0300

    finalizando aula DOM

--- m1-fundamentos/b5-dom_eventos_web-storage/aula1/aula.js
+++ m1-fundamentos/b5-dom_eventos_web-storage/aula1/aula.js
@@ -2,8 +2,8 @@
 //console.log(document.getElementById("teste2").innerHTML);
 // é possível alterar o conteúdo de uma tag através dessa propriedade
 //document.getElementById("teste").innerHTML = "Aprendizados da aula de hoje";
-let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
+// let pilotosDeF1 = document.getElementsByClassName("piloto-f1-atual");
 
-for (let i = 0; i < pilotosDeF1.length; i += 1) {
-    pilotosDeF1[i].innerText = "Lewis Hamilton";
-}
+// for (let i = 0; i < pilotosDeF1.length; i += 1) {
+//     pilotosDeF1[i].innerText = "Lewis Hamilton";
+// }
