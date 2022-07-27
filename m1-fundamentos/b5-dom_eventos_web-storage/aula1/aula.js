c73ff4f9b86edd30f08d8e60369b65db0f030dab
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-07-27 09:52:20 -0300

    iniciando DOM

---
+++ m1-fundamentos/b5-dom_eventos_web-storage/aula1/aula.js
@@ -0,0 +1,4 @@
+//console.log(document.getElementById("teste").innerText);
+//console.log(document.getElementById("teste2").innerHTML);
+// é possível alterar o conteúdo de uma tag através dessa propriedade
+//document.getElementById("teste").innerHTML = "Aprendizados da aula de hoje";
