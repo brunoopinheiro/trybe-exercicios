3d0af676528b77f3602cf10f7c8f9664bb8fc3e6
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-07-28 11:34:41 -0300

    aprofundamentos - partes 7 a 9

--- m1-fundamentos/b5-dom_eventos_web-storage/b5-d2/aprofunde/script.js
+++ m1-fundamentos/b5-dom_eventos_web-storage/b5-d2/aprofunde/script.js
@@ -29,3 +29,25 @@ main.appendChild(leftContent);
 let rightContent = document.createElement('section');
 rightContent.className = 'right-content';
 main.appendChild(rightContent);
+
+// 7
+let image = document.createElement('img');
+image.src = 'https://picsum.photos/200';
+image.className = 'small-image';
+leftContent.appendChild(image);
+
+// 8
+let listaValores = ['um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez'];
+let listaNaoOrdenada = document.createElement('ul');
+rightContent.appendChild(listaNaoOrdenada);
+for (let i = 0; i < listaValores.length; i += 1) {
+    let valor = document.createElement('li');
+    valor.innerHTML = listaValores[i];
+    listaNaoOrdenada.appendChild(valor);
+}
+
+// 9
+for (let j = 0; j < 3; j += 1) {
+    let h3 = document.createElement('h3');
+    main.appendChild(h3);
+}
