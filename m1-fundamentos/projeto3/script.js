311f9c055ce464017c432be27a10a2adfacba82c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-08-01 21:22:27 -0300

    requisitos 6 - 8

--- m1-fundamentos/projeto3/script.js
+++ m1-fundamentos/projeto3/script.js
@@ -1,10 +1,54 @@
 const pixelBoard = document.getElementById('pixel-board');
+const blackSelector = document.getElementById('black-selector');
+const redSelector = document.getElementById('red-selector');
+const blueSelector = document.getElementById('blue-selector');
+const greenSelector = document.getElementById('green-selector');
+
+function selectColor(event) {
+    const colorSelectors = document.getElementsByClassName('color');
+    for (let i = 0; i < colorSelectors.length; i += 1) {
+        if (colorSelectors[i].classList.contains('selected')) {
+            colorSelectors[i].classList.remove('selected');
+        }
+   }
+   event.target.classList.add('selected');
+}
+
+function paint(event) {
+    const selector = document.querySelector('.selected');
+    console.log(selector.style.backgroundColor);
+    let color = selector.style.backgroundColor;
+    if (event.target.style.backgroundColor !== 'white') {
+        event.target.style.backgroundColor = 'white';
+    } else {
+        event.target.style.backgroundColor = color;
+    }
+}
 
 for (let i = 0; i < 5; i += 1) {
-    for (let j = 0; j < 5; j += 1){
+    for (let j = 0; j < 5; j += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
         pixel.style.backgroundColor = 'white';
         pixelBoard.appendChild(pixel);
     }
 }
+
+window.onload = function () {
+    blackSelector.classList.add('selected');
+    blackSelector.style.backgroundColor = 'black';
+    redSelector.style.backgroundColor = 'red';
+    blueSelector.style.backgroundColor = 'blue';
+    greenSelector.style.backgroundColor = 'green';
+}
+
+const pixels = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixels.length; i += 1) {
+    const pixel = pixels[i];
+    pixel.addEventListener('click', paint);
+}
+
+blackSelector.addEventListener('click', selectColor);
+redSelector.addEventListener('click', selectColor);
+blueSelector.addEventListener('click', selectColor);
+greenSelector.addEventListener('click', selectColor);
