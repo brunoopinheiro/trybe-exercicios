e080be1e6597f90c477cd91a63f6b0104793f7c0
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-08-12 13:27:03 -0300

    resolvendo e3 em 1 linha

--- m1-fundamentos/b7-javascriptES6-testes_unitarios/exercicios/ex3.js
+++ m1-fundamentos/b7-javascriptES6-testes_unitarios/exercicios/ex3.js
@@ -1,10 +1,12 @@
-const factorial = n => {
-  let resultado = n;
-  for (let i = n - 1; i > 0; i -= 1) {
-    resultado = resultado * i;
-  }
-  return resultado;
-}
+// const factorial = n => {
+//   let resultado = n;
+//   for (let i = n - 1; i > 0; i -= 1) {
+//     resultado = resultado * i;
+//   }
+//   return resultado;
+// }
+
+const factorial = n => (n > 0 ? n * factorial(n - 1) : 1);
 
 const a = 4;
 console.log(`Esse e o fatorial de ${a}: ${factorial(a)} `);
