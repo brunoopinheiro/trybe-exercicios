1fa25dab562ea5cc1709498a6cd96067d0c79d94
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-08-25 13:27:10 -0300

    ex 7

--- m1-fundamentos/b8-higher_order_functions_JSES6/b8d4-spread_rest_destructuring/pratica/ex5.js
+++ m1-fundamentos/b8-higher_order_functions_JSES6/b8d4-spread_rest_destructuring/pratica/ex5.js
@@ -13,3 +13,28 @@ const toObject = ([modelo, montadora, anoLancamento]) => ({ modelo, montadora, a
 console.log(toObject(palio));
 console.log(toObject(shelbyCobra));
 console.log(toObject(chiron));
+
+// 7 - funcao shipLength() dado objeto navio retorna string `Ship is 000.0 measurementUnit long.`
+const ships = [
+  {
+    name: 'Titanic',
+    length: 269.1,
+    measurementUnit: 'meters',
+  },
+  {
+    name: 'Queen Mary 2',
+    length: 1132,
+    measurementUnit: 'feet',
+  },
+  {
+    name: 'Yamato',
+    length: 256,
+    measurementUnit: 'meters',
+  },
+];
+
+const shipLength = ({ name, length, measurementUnit }) => `${name} is ${length} ${measurementUnit} long.`
+
+console.log(shipLength(ships[0]));
+console.log(shipLength(ships[1]));
+console.log(shipLength(ships[2]));
