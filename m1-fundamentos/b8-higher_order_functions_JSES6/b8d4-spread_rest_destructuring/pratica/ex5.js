2729fd04e23a60e94d78ac1780c21a8d22039622
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-08-25 13:17:58 -0300

    ex 6

--- m1-fundamentos/b8-higher_order_functions_JSES6/b8d4-spread_rest_destructuring/pratica/ex5.js
+++ m1-fundamentos/b8-higher_order_functions_JSES6/b8d4-spread_rest_destructuring/pratica/ex5.js
@@ -2,3 +2,14 @@ const myList = [5, 2, 3];
 
 const swap = ([el3, el2, el1]) => [el1, el2, el3];
 console.log(swap(myList));
+
+// 6 - Alterar array carros para objetos.
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugattin', 2016];
+
+const toObject = ([modelo, montadora, anoLancamento]) => ({ modelo, montadora, anoLancamento });
+
+console.log(toObject(palio));
+console.log(toObject(shelbyCobra));
+console.log(toObject(chiron));
