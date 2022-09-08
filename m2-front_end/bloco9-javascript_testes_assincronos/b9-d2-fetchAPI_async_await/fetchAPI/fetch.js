16aa811c445b5407a22483fa7a443c6c37bd4f44
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-08 10:29:15 -0300

    fetchAPI

---
+++ m2-front_end/bloco9-javascript_testes_assincronos/b9-d2-fetchAPI_async_await/fetchAPI/fetch.js
@@ -0,0 +1,12 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
