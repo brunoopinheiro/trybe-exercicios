9d06ef26c5b21f2279fc432d61c95d72a6733c93
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-08 11:20:27 -0300

    async await

--- m2-front_end/bloco9-javascript_testes_assincronos/b9-d2-fetchAPI_async_await/fetchAPI/fetch.js
+++ m2-front_end/bloco9-javascript_testes_assincronos/b9-d2-fetchAPI_async_await/fetchAPI/fetch.js
@@ -1,12 +1,27 @@
 const fetch = require('node-fetch');
 
-const fetchJoke = () => {
+const fetchJoke = async () => {
   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
 
-  fetch(url)
+  const result = await fetch(url)
     .then((response) => response.json())
-    .then((data) => console.log(data.value))
+    .then((data) => data.value)
     .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+
+  console.log(result);
+}
+
+const fetchJokeTryCatch = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch (error) {
+    console.log(`Algo deu errado :( \n${error}`);
+  }
 }
 
 fetchJoke();
+fetchJokeTryCatch();
