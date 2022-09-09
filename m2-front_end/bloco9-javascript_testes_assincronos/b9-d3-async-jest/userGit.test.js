bfaecff2922cd06750384242c65fc7a793af5853
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-09 17:42:14 -0300

    finalizando exemplo da aula

--- m2-front_end/bloco9-javascript_testes_assincronos/b9-d3-async-jest/userGit.test.js
+++ m2-front_end/bloco9-javascript_testes_assincronos/b9-d3-async-jest/userGit.test.js
@@ -1,16 +1,29 @@
 const { getUserGit, getUserInfo } = require('./scripts');
+const { VALID_URL, VALID_USERNAME, fetchMock} = require('./fetchMock');
 const { expect } = require('@jest/globals');
 
 const expected = {
   name: 'Thiago "BRADDOCK" Oliveira',
-  bio: 'Professor, Programador, Montanhista, Jogador de CS, Karateca, entusiasta metido a cozinheiro e sommelier.',
-  location: 'Espírito Santo do Pinhal, SP - Brasil',
-  company: '@betrybe',
+  company: 'GreenCatDev',
+  bio: 'Professor, Programador, Jogador de CS, Karateca, entusiasta metido a cozinheiro e sommelier.',
+  location: 'Espírito Santo do Pinhal',
 }
 
 describe('Test scripts.js', () => {
   it('tests getUserInfo', async () => {
     expect.assertions(1);
     expect(await getUserInfo('thiagobraddock')).toEqual(expected);
+  });
+
+  it('verifies if fetch have been called', async () => {
+    expect.assertions(1);
+    await getUserInfo(VALID_USERNAME);
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('verifies if fetch was called with the valid url', async () => {
+    expect.assertions(1);
+    await getUserInfo(VALID_USERNAME);
+    expect(fetch).toHaveBeenCalledWith(VALID_URL);
   });
 });
