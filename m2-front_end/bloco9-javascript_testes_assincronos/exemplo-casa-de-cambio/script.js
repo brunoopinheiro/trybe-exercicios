e8ce7c390ab11e67b3837ea733cc86ebf66aa13d
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-14 17:14:27 -0300

    manipula local storage

--- m2-front_end/bloco9-javascript_testes_assincronos/exemplo-casa-de-cambio/script.js
+++ m2-front_end/bloco9-javascript_testes_assincronos/exemplo-casa-de-cambio/script.js
@@ -2,12 +2,39 @@ const btnPesquisar = document.getElementById('search-button');
 const input = document.getElementById('currency-input');
 const currenciesList = document.getElementById('currency-list');
 
+const createFavoriteItem = (moeda, valor) => {
+  const favoriteList = document.getElementById('favorite_currencies');
+
+  const li = document.createElement('li');
+
+  const div = document.createElement('div');
+  div.append(moeda);
+  
+  li.appendChild(div);
+  li.append(valor);
+
+  favoriteList.appendChild(li);
+};
+
+const addItemToLocalStorage = () => {
+  const favoriteList = document.getElementById('favorite_currencies');
+
+  localStorage.setItem('moedas', favoriteList.innerHTML);
+};
+
+const getItemsFromLocalStorage = () => localStorage.getItem('moedas');
+
 const createList = ([moeda, valor]) => {
   const li = document.createElement('li');
   const span = document.createElement('span');
   span.id = moeda;
   span.className = 'material-symbols-outlined heart';
   span.innerHTML = 'favorite';
+
+  span.addEventListener('click', () => {
+    createFavoriteItem(moeda, valor);
+    addItemToLocalStorage();
+  });
 
   const div = document.createElement('div');
   div.append(moeda);
@@ -33,3 +60,9 @@ btnPesquisar.addEventListener('click', async () => {
 
   array.forEach(createList);
 })
+
+window.onload = () => {
+  const favoriteList = document.getElementById('favorite_currencies');
+  const moedasFavoritas = getItemsFromLocalStorage();
+  favoriteList.innerHTML = moedasFavoritas;
+};
