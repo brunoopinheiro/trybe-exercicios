d1c7124ed2147db292cb9391de2bda10cccdc223
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-14 15:33:32 -0300

    iniciando fetch da api

--- m2-front_end/bloco9-javascript_testes_assincronos/exemplo-casa-de-cambio/api.js
+++ m2-front_end/bloco9-javascript_testes_assincronos/exemplo-casa-de-cambio/api.js
@@ -1,6 +1,22 @@
+// https://api.exchangerate.host/latest?base=BRL
+const URL_API = 'https://api.exchangerate.host/latest';
 
-if (typeof module !== 'undefined') {
-  module.exports = {
-    fetchCurrencies,
-  };
-}
+const fetchCurrencies = async (moeda) => {
+  try{
+    const endpoint = `${URL_API}?base=${moeda}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data.rates;
+  } catch (error) {
+
+  }
+};
+
+fetchCurrencies('BRL')
+  .then(devolvido => console.log(devolvido));
+
+// if (typeof module !== 'undefined') {
+//   module.exports = {
+//     fetchCurrencies,
+//   };
+// }
