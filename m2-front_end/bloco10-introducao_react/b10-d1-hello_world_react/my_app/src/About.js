1879cee47503d8311d22c12ce747d07b69a9169f
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-19 10:55:03 -0300

    first react app

---
+++ m2-front_end/bloco10-introducao_react/b10-d1-hello_world_react/my_app/src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return <div>
+      <h1>Bruno Pinheiro</h1>
+      <p>Dungeon Master, Archaeologist and Web Developer student.</p>
+    </div>
+  }
+}
+
+export default About;
