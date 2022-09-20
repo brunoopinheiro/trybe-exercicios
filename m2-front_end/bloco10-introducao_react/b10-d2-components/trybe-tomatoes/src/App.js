fd9dc1b9bdb26fffc80648b7bdb29faae5a3c3a9
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-20 18:04:58 -0300

    finaliza exemplo aula

--- m2-front_end/bloco10-introducao_react/b10-d2-components/trybe-tomatoes/src/App.js
+++ m2-front_end/bloco10-introducao_react/b10-d2-components/trybe-tomatoes/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import CardList from './components/CardList';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        dale
+        <CardList className="Series__container" />
       </div>
     );
   }
