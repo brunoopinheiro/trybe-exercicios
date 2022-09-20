892979e684fa09c37cb98264e9b03c9712bf09a5
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-20 11:29:55 -0300

    defaultProps

--- m2-front_end/bloco10-introducao_react/b10-d2-components/exercicio1/src/components/UserName.js
+++ m2-front_end/bloco10-introducao_react/b10-d2-components/exercicio1/src/components/UserName.js
@@ -1,10 +1,19 @@
 import { Component } from "react";
 import './UserName.css';
+import PropTypes from 'prop-types';
 
 class UserName extends Component {
   render() {
     return <span className="name">{ this.props.name }</span>
   }
+}
+
+UserName.propTypes = {
+  name: PropTypes.string,
+}
+
+UserName.defaultProps = {
+  name: 'John Doe',
 }
 
 export default UserName;
