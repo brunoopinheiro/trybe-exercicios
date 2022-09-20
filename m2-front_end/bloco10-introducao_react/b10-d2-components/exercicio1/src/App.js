3e94bf14f98960f3f56ecbf7e56db5650655f90f
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-20 09:37:50 -0300

    image exercise

---
+++ m2-front_end/bloco10-introducao_react/b10-d2-components/exercicio1/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Image from './Image';
+
+function App() {
+  return (
+    <div className="App">
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+    </div>
+  );
+}
+
+export default App;
