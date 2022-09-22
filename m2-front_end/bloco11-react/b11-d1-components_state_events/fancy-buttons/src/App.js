8cffedec1f88bd5310a066f2d8842c4f5162a6c9
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-09-22 12:04:26 -0300

    lesson: states and events

--- m2-front_end/bloco11-react/b11-d1-components_state_events/fancy-buttons/src/App.js
+++ m2-front_end/bloco11-react/b11-d1-components_state_events/fancy-buttons/src/App.js
@@ -7,9 +7,16 @@ class App extends React.Component {
     super();
 
     this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      numeroDeCliques: 0,
-    }
+
+    // Utilizando sintaxe Public Class Field e removendo de dentro do construtor.
+    // this.state = {
+    //   numeroDeCliques: 0,
+    // }
+  }
+
+  // Definindo estado utilizando sintaxe Public Class Field
+  state = {
+    numeroDeCliques: 0,
   }
 
   // Ao definir uma funcao da classe com uma arrow function, com a sintaxe minhaFuncao = () => {...}, voce nao precisara fazer o bind. Entao nao precisaremos do construtor nesse caso. Veja como o exemplo acima seria feito com arrow function.
@@ -17,6 +24,7 @@ class App extends React.Component {
     this.setState((estadoAnterior, _props) => ({
       numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
     }))
+    this.state.numeroDeCliques % 2 === 0 ? console.log('verde') : console.log('vermelho');
     console.log('click');
   }
 
