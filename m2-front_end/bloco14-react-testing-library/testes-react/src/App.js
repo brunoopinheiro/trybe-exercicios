d361eddea9a196d2a2fa1f0ed2cef623f4d65ef2
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-10-14 11:27:17 -0300

    lesson: tests with react testing library

--- m2-front_end/bloco14-react-testing-library/testes-react/src/App.js
+++ m2-front_end/bloco14-react-testing-library/testes-react/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ValidEmail from './ValidEmail';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      saveEmail: '',
+    };
+  }
+
+  handleInput = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  changeSaveEmail = () => {
+    const { email } = this.state;
+    this.setState({ saveEmail: email, email: '' });
+  }
+
+  render() {
+    const { email, saveEmail } = this.state;
+
+    return(
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+          <input
+            id="id-email"
+            value={ email }
+            type="email"
+            name="email"
+            onChange={ this.handleInput }
+          />
+        </label>
+        <input
+          id="btn-enviar"
+          type="button"
+          data-testid="id-send"
+          value="Enviar"
+          onClick={ this.changeSaveEmail }
+        />
+        <input
+          id="btn-id"
+          type="button"
+          value="Voltar"
+        />
+        <ValidEmail email={ saveEmail } />
+      </div>
+    );
+  }
 }
 
 export default App;
--- m2-front_end/bloco14-react-testing-library/testes-react/src/App.test.js
+++ m2-front_end/bloco14-react-testing-library/testes-react/src/App.test.js
@@ -1,8 +1,52 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/outra coisa/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Tela de Insercao de Email', () => {
+  test('Verifica se existe um input de email na tela', () => {
+    // Acessar os elementos da tela
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    
+    // Fazer os testes
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputEmail.type).toBe('email');
+  });
+  
+  test('Verifica se existem dois botoes na tela', () => {
+    // Acessar os elementos da tela
+    render(<App />);
+    const buttons = screen.getAllByRole('button');
+  
+    // Fazer os testes
+    expect(buttons).toHaveLength(2);
+  });
+  
+  test('Verifica se existe o botao de "Enviar"', () => {
+    render(<App />);
+    const button = screen.getByTestId('id-send');
+  
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveValue('Enviar');
+  });
+  
+  test('Verifica que, ao digitar o email e clicar em "Enviar", o email e renderizado', () => {
+    render(<App />);
+    const inputEmail = screen.getByLabelText('Email');
+    const button = screen.getByTestId('id-send');
+    const userEmail = screen.getByTestId('id-email-user');
+  
+    // Interagir com os elementos (se for necessario)
+    userEvent.type(inputEmail, 'teste@teste.com');
+    userEvent.click(button);
+  
+    expect(inputEmail).toHaveValue('');
+    expect(userEmail).toHaveTextContent('Valor: teste@teste.com');
+  });
 });
+
+  // Acessar os elementos da tela
+
+  // Interagir com os elementos (se for necessario)
+
+  // Fazer os testes
