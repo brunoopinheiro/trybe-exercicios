1e628d8a1bdfcbeb0463beef413e3dd5899dfb47
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-10-17 11:58:37 -0300

    exercise: api mock tests

--- m2-front_end/bloco14-react-testing-library/mock-piadas/src/App.test.js
+++ m2-front_end/bloco14-react-testing-library/mock-piadas/src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 afterEach(() => jest.clearAllMocks());
@@ -35,3 +36,42 @@ it('fetches a joke', async () => {
     { headers: { Accept: 'application/json' } },
   );
 })
+
+it('tests if the "New Joke" button works as intended', async () => {
+  const joke1 = {
+    id: '7h3oGtrOfxc',
+    joke: 'Unique Joke',
+    status: 200,
+  };
+  const joke2 = {
+    id: '7h3oGtrOfxa',
+    joke: 'Another thing',
+    status: 200,
+  };
+
+  jest.spyOn(global, 'fetch');
+  global.fetch
+    .mockResolvedValue({
+      json: jest.fn().mockResolvedValue(joke2),
+    })
+    .mockResolvedValueOnce({
+      json: jest.fn().mockResolvedValue(joke1),
+    });
+
+  render(<App />);
+  const renderedJoke = await screen.findByText(joke1.joke);
+  expect(renderedJoke).toBeInTheDocument();
+  expect(renderedJoke).toHaveTextContent(joke1.joke);
+  expect(global.fetch).toHaveBeenCalledTimes(1);
+
+  const newJokeButton = await screen.findByRole('button');
+  expect(newJokeButton).toBeInTheDocument();
+  userEvent.click(newJokeButton);
+  expect(global.fetch).toHaveBeenCalledTimes(2);
+  const newJoke = await screen.findByText(joke2.joke);
+  expect(newJoke).toBeInTheDocument();
+  expect(newJoke).toHaveTextContent(joke2.joke);
+  
+  // Comportamento curioso.
+  expect(renderedJoke).not.toHaveTextContent(joke1.joke);
+});
