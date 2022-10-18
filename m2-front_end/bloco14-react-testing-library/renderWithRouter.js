2a279ca5e393b373773fa6ab393c249c1848b373
Bruno Pinheiro <93209918+brunoopinheiro@users.noreply.github.com>
2022-10-18 10:03:28 -0300

    Create model function renderWithRouter

---
+++ m2-front_end/bloco14-react-testing-library/renderWithRouter.js
@@ -0,0 +1,16 @@
+// Model function to work with Browser Router in RTL
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
+const renderWithRouter = (component) => {
+  const history = createMemoryHistory();
+  return({
+    ...render(<Router history={history}>{component}</Router>),
+    history,
+  });
+};
+
+export default renderWithRouter;
