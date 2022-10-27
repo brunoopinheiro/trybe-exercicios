5740ce4b135741d5c67594d1c0d8997d4759f38e
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-10-27 11:47:00 -0300

    starting development - setup project

---
+++ m2-front_end/bloco15-redux/got-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>
+        GoT
+      </p>
+      <iframe width="1344" height="526" src="https://www.youtube.com/embed/RfI_mUIvYq8" title="Game of Thrones: The Musical – Emilia Clarke Teaser | Red Nose Day" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  );
+}
+
+export default App;
