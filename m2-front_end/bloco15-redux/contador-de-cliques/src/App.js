221d97656f0beb72374b6285e0006b25ff5c668c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-10-28 09:54:34 -0300

    lesson: tests with redux

--- m2-front_end/bloco15-redux/contador-de-cliques/src/App.js
+++ m2-front_end/bloco15-redux/contador-de-cliques/src/App.js
@@ -1,18 +1,14 @@
 import './App.css';
 import React from 'react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import ButtonClicks from './components/ButtonClicks';
 import NumberClicks from './components/NumberClicks';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <Provider store={ store }>
+      <div className="App">
           <ButtonClicks />
           <NumberClicks />
-        </Provider>
       </div>
     );
   }
--- m2-front_end/bloco15-redux/contador-de-cliques/src/App.test.js
+++ m2-front_end/bloco15-redux/contador-de-cliques/src/App.test.js
@@ -1,1 +1,42 @@
-it.todo('todo');
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+import { legacy_createStore as createStore, combineReducers } from 'redux';
+import clickReducer from './redux/reducers';
+
+const renderWithRedux = (
+  component,
+  {
+    initialState,
+    store = createStore(combineReducers({ clickReducer }), initialState),
+  } = {}
+) => ({
+  ...render(<Provider store={store}>{component}</Provider>),
+  store,
+});
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+
+  test('the page should have a button and a text 0', () => {
+    renderWithRedux(<App />);
+    const buttonAdicionar = screen.queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(screen.getByText('0')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
+
+    expect(screen.getByText('5')).toBeInTheDocument();
+
+    const button = screen.queryByRole('button');
+    expect(button).toHaveTextContent('Clique aqui');
+
+    userEvent.click(button);
+    expect(screen.getByText('6')).toBeInTheDocument();
+  });
+});
