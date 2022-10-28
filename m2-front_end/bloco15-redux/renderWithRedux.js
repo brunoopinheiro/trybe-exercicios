0afc355b2183eb328b9337a93cb0331d2a7c3167
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2022-10-28 09:57:08 -0300

    update renderWithRedux to async uses

--- m2-front_end/bloco15-redux/renderWithRedux.js
+++ m2-front_end/bloco15-redux/renderWithRedux.js
@@ -1,7 +1,14 @@
 // Funcao modelo renderWithRedux
 const renderWithRedux = (
   component,
-  { initialState, store = createStore(reducer, initialState) } = {}
+  {
+    initialState,
+    store = createStore(
+      reducer,
+      initialState,
+      applyMiddleware(thunk) /* Necessario apenas para testes assincronos */
+    ),
+  } = {}
 ) => {
   return {
     ...render(<Provider store={store}>{component}</Provider>),
