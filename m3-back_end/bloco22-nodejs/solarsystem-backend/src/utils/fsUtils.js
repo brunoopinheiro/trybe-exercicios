090426fd3f92f5aaf78a2ed094d305c8b6fd5d3c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-01-30 12:16:22 -0300

    end of lesson

--- m3-back_end/bloco22-nodejs/solarsystem-backend/src/utils/fsUtils.js
+++ m3-back_end/bloco22-nodejs/solarsystem-backend/src/utils/fsUtils.js
@@ -1,9 +1,11 @@
 const fs = require('fs').promises;
 const path = require('path');
 
+const MISSION_DATA_PATH = '../../data/missions.json';
+
 async function readMissionsData() {
   try {
-    const data = await fs.readFile(path.resolve(__dirname, '../../data/missions.json'));
+    const data = await fs.readFile(path.resolve(__dirname, MISSION_DATA_PATH));
     const missions = JSON.parse(data);
     
     return missions;
@@ -12,4 +14,18 @@ async function readMissionsData() {
   }
 }
 
-module.exports = { readMissionsData };
+async function writeNewMissionData(newMission) {
+  try {
+      const oldMissions = await readMissionsData();
+      const allMisssions = JSON.stringify([...oldMissions, { id: Date.now(), ...newMission }]);
+
+      await fs.writeFile(path.resolve(__dirname, MISSION_DATA_PATH), allMisssions);
+  } catch (error) {
+    console.error(`Erro na escrita do arquivo: ${error}`);
+  }
+}
+
+module.exports = {
+  readMissionsData,
+  writeNewMissionData,
+};
