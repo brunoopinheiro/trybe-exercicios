d093f30c338457209aa77c21339a20ddb20952d1
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-01-30 13:48:38 -0300

    exercise 5

--- m3-back_end/bloco22-nodejs/my-scripts/bmi.js
+++ m3-back_end/bloco22-nodejs/my-scripts/bmi.js
@@ -1,6 +1,33 @@
 // imc = peso / altura^2
 const resync = require('readline-sync');
 
+const BMI_INDEX = {
+  'Underweight': {
+    minBMI: 0,
+    maxBMI: 18.4,
+  },
+  'Normal Weigth': {
+    minBMI: 18.5,
+    maxBMI: 24.9,
+  },
+  'Overweight': {
+    minBMI: 25,
+    maxBMI: 29.9,
+  },
+  'Obese Class I': {
+    minBMI: 30,
+    maxBMI: 34.9,
+  },
+  'Obese Class II': {
+    minBMI: 35,
+    maxBMI: 39.9,
+  },
+  'Obese Class III': {
+    minBMI: 40,
+    maxBMI: 100,
+  },
+}
+
 function bmi(weight, height) {
   try {
     return weight / (height ^ 2);
@@ -10,11 +37,19 @@ function bmi(weight, height) {
 };
 
 function main() {
-  const weight = resync.questionInt('Enter your weight (kg): ');
-  const height = resync.questionInt('Enter your height (m): ');
+  const weight = resync.questionFloat('Enter your weight (kg): ');
+  const height = resync.questionFloat('Enter your height (m): ');
   const bmiResult = bmi(weight, height);
 
+  const statuses = Object.keys(BMI_INDEX);
+  const resultFind = statuses.find((status) => {
+    const { maxBMI, minBMI } = BMI_INDEX[status];
+
+    return bmiResult >= minBMI && bmiResult <= maxBMI;
+  })
+
   console.log(`BMI: ${bmiResult}`);
+  console.log(resultFind);
 }
 
 main();
