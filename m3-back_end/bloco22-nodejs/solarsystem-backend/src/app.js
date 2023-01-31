5d0056abdddf65aeb70879384f0b5903660c6cfa
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-01-31 13:55:37 -0300

    feat REST api

--- m3-back_end/bloco22-nodejs/solarsystem-backend/src/app.js
+++ m3-back_end/bloco22-nodejs/solarsystem-backend/src/app.js
@@ -1,5 +1,43 @@
 const express = require('express');
+const {
+  readMissionsData,
+  writeNewMissionData,
+  updateMissionData,
+  deleteMissionData,
+} = require('./utils/fsUtils');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/missions', async (_req, res) => {
+  const missions = await readMissionsData();
+
+  return res.status(200).json({ missions });
+});
+
+app.post('/missions', async (req, res) => {
+  const newMission = req.body;
+
+  const newMissionWithId = await writeNewMissionData(newMission);
+
+  return res.status(201).json({ mission: newMissionWithId });
+});
+
+app.put('/missions/:id', async (req, res) => {
+  const { id } = req.params;
+  const updatedMissionData = req.body;
+
+  const updatedMission = await updateMissionData(Number(id), updatedMissionData);
+
+  return res.status(201).json({ mission: updatedMission });
+});
+
+app.delete('/missions/:id', async (req, res) => {
+  const { id } = req.params;
+  await deleteMissionData(Number(id));
+
+  return res.status(204).end();
+});
 
 module.exports = app;
