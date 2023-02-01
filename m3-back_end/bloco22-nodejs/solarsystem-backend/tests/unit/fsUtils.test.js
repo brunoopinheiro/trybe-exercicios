9a9b7d4a005a860b117e8993045de76fa3d693e6
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-01 14:43:28 -0300

    tests: adding sinon to mock

--- m3-back_end/bloco22-nodejs/solarsystem-backend/tests/unit/fsUtils.test.js
+++ m3-back_end/bloco22-nodejs/solarsystem-backend/tests/unit/fsUtils.test.js
@@ -1,11 +1,22 @@
 const { expect } = require('chai');
 
+const fs = require('fs');
+const sinon = require('sinon');
 const { readMissionsData } = require('../../src/utils/fsUtils');
 
+const mockMissions = JSON.stringify([
+  { id: 1, name: 'Mariner 2', year: '1962', country: 'EUA', destination: 'Vênus' },
+  { id: 2, name: 'Venera 4', year: '1967', country: 'URSS', destination: 'Vênus' },
+  { id: 3, name: 'Mariner 5', year: '1967', country: 'EUA', destination: 'Vênus' },
+]);
+
 describe('readMissionsData function', function () {
-  it('returns an array', async function () {
+  it('returns an array with all json elements', async function () {
+    sinon.stub(fs.promises, 'readFile').resolves(mockMissions);
     const missions = await readMissionsData();
 
     expect(missions).to.be.instanceOf(Array);
+    expect(missions).to.have.lengthOf(3);
+    sinon.restore();
   });
 });
