6cd3e387646f842fdf77ac7f765076bc50647c79
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-02 14:06:06 -0300

    feat: adding CORS

--- m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
+++ m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
+const cors = require('cors');
 const validateTeam = require('./middlewares/validateTeam');
 const existingId = require('./middlewares/existingId');
 const apiCredentials = require('./middlewares/apiCredentials');
@@ -10,6 +11,7 @@ const teams = require('./data/teams');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(apiCredentials);
 
