9aeff3057b8c0ad3253bd6655c1154ca01e14230
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-02 11:04:05 -0300

    refactor: applying middlewares

--- m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
+++ m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
@@ -17,6 +17,16 @@ const teams = [
 const app = express();
 app.use(express.json());
 
+// Middlewares
+const validateTeam = (req, res, next) => {
+  const requiredProperties = ['nome', 'sigla'];
+  if (requiredProperties.every((property) => property in req.body)) {
+    next();
+  } else {
+    res.sendStatus(400);
+  }
+};
+
 // Routes
 
 // app.get('/', (req, res) => res.status(200).json({ message: 'Hello, world!' }));
@@ -35,23 +45,17 @@ app.get('/teams/:id', (req, res) => {
   }
 });
 
-app.post('/teams', (req, res) => {
-  const requiredProperties = ['nome', 'sigla'];
-  if (requiredProperties.every((property) => property in req.body)) {
-    const team = { id: nextId, ...req.body };
-    teams.push(team);
-    nextId += 1;
-    res.status(201).json(team);
-  } else {
-    res.sendStatus(400);
-  }
+app.post('/teams', validateTeam, (req, res) => {
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
 });
 
-app.put('/teams/:id', (req, res) => {
+app.put('/teams/:id', validateTeam, (req, res) => {
   const id = Number(req.params.id);
-  const requiredProperties = ['nome', 'sigla'];
   const team = teams.find((t) => t.id === id);
-  if (team && requiredProperties.every((property) => property in req.body)) {
+  if (team) {
     const index = teams.indexOf(team);
     const updated = { id, ...req.body };
     teams.splice(index, 1, updated);
