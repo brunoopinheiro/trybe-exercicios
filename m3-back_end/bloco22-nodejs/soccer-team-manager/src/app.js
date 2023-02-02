93e6b7829b691beaa44bec5529a35228dbe6679c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-02 15:08:47 -0300

    clearing comments

--- m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
+++ m3-back_end/bloco22-nodejs/soccer-team-manager/src/app.js
@@ -3,23 +3,14 @@ require('express-async-errors');
 const morgan = require('morgan');
 const cors = require('cors');
 const teamsRouter = require('./routes/teamsRouter');
-// const validateTeam = require('./middlewares/validateTeam');
-// const existingId = require('./middlewares/existingId');
-// const apiCredentials = require('./middlewares/apiCredentials');
-
-// const nextId = 3;
-// const teams = require('./data/teams');
 
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use('/teams', teamsRouter);
-// app.use(apiCredentials);
 
 // Routes
-
-// app.get('/', (req, res) => res.status(200).json({ message: 'Hello, world!' }));
 
 app.use((err, _req, _res, next) => {
   console.error(err.stack);
