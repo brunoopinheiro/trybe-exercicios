52471046e814fb1b70aba98b849c2c6649c13dbd
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-02 13:10:20 -0300

    fix: remove console.log

--- m3-back_end/bloco22-nodejs/soccer-team-manager/src/middlewares/apiCredentials.js
+++ m3-back_end/bloco22-nodejs/soccer-team-manager/src/middlewares/apiCredentials.js
@@ -6,7 +6,6 @@ const AUTHDATA_PATH = '../authdata.json';
 module.exports = async function apiCredentials(req, res, next) {
   // pega o token do header, se houver
   const token = req.header('X-API-TOKEN');
-  console.log(`Token: ${token}, Type: ${typeof token}`);
   // le o conteudo do authData
   const authdata = await fs.readFile(path.resolve(__dirname, AUTHDATA_PATH), { encoding: 'utf-8' });
   // readFile nos deu uma string, vamos carregar um objeto a partir dela
