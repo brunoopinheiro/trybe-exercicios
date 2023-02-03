02f9380b91233ee18e302cb4d733eb7a3af59c1e
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-03 11:40:19 -0300

    fix typos

--- m3-back_end/bloco22-nodejs/trybecash-api/src/server.js
+++ m3-back_end/bloco22-nodejs/trybecash-api/src/server.js
@@ -1,13 +1,7 @@
 const app = require('./app');
-const connection = require('./db/connection');
 
 const PORT = 3000;
 
 app.listen(PORT, async () => {
   console.log(`A API TrybeCash está sendo executada na porta ${PORT}`);
-
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL Connection OK');
-  }
 });
