af04503d2160072d207a928af24ccea8e362b3e1
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-03 16:40:05 -0300

    adding enviroment variables

--- m3-back_end/bloco22-nodejs/trybecash-api/src/server.js
+++ m3-back_end/bloco22-nodejs/trybecash-api/src/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const app = require('./app');
 
 const PORT = 3001;
 
 app.listen(PORT, async () => {
   console.log(`A API TrybeCash está sendo executada na porta ${PORT}`);
+  console.log(`Valor da variavel de ambiente $USER: ${process.env.USER}`);
 });
