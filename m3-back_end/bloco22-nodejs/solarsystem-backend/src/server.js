10d6a400bd3030b63d4e288f2fe579b3465deed4
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-03 12:34:19 -0300

    starting to add db to application

--- m3-back_end/bloco22-nodejs/solarsystem-backend/src/server.js
+++ m3-back_end/bloco22-nodejs/solarsystem-backend/src/server.js
@@ -1,3 +1,10 @@
 const app = require('./app');
+const connection = require('./db/connection');
 
-app.listen(3001, () => console.log('Back-end rodando na porta 3001'));
+app.listen(3001, async () => {
+  console.log('Back-end rodando na porta 3001');
+  const [result] = await connection.execute('SELECT * FROM missions');
+
+  console.log(result);
+  // if (result) console.log('MySQL Connect OK!');
+});
