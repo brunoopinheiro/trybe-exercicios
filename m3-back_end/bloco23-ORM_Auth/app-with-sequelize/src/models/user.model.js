6b6196d44409d5fc022fc9939ae0989d2de4397c
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-27 11:28:33 -0300

    create: user model

---
+++ m3-back_end/bloco23-ORM_Auth/app-with-sequelize/src/models/user.model.js
@@ -0,0 +1,16 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    fullName: DataTypes.STRING,
+    email: DataTypes.STRING,
+  });
+  (async () => {
+    // sync method is being called. This means that everytime the server is initialized, the table will be recreated.
+    // This is good for tests, but should NOT be used in production.
+    await sequelize.sync({ force: true });
+    // Functions here
+  })();
+
+  return User;
+};
+
+module.exports = UserModel;
