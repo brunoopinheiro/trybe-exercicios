7b6e717f5160096b0ccfda96d7c183f291c3af8d
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-27 15:41:14 -0300

    add migration - create Books table

--- m3-back_end/bloco23-ORM_Auth/biblioteca-orm/src/migrations/20230227183121-create-books-table.js
+++ m3-back_end/bloco23-ORM_Auth/biblioteca-orm/src/migrations/20230227183121-create-books-table.js
@@ -2,20 +2,37 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('Books', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Books');
   }
 };
