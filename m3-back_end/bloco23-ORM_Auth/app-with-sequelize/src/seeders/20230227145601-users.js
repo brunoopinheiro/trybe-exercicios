1f798ad15ea6ae98645c6fcc5c86debe3c3a8cc3
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-27 12:02:44 -0300

    creating seeders

---
+++ m3-back_end/bloco23-ORM_Auth/app-with-sequelize/src/seeders/20230227145601-users.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   queryInterface.bulkInsert('Users', [
+    {
+      fullName: 'Leonardo',
+      email: 'leo@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+    {
+      fullName: 'JEduardo',
+      email: 'edu@test.com',
+      createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      updatedAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+    },
+   ], {});
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    queryInterface.bulkDelete('Users', null, {});
+  }
+};
