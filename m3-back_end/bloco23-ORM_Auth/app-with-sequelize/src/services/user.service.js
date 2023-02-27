f7d8133316683f0aa1a2d031eca23c8317e74835
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-02-27 12:29:52 -0300

    feat: CRUD user

--- m3-back_end/bloco23-ORM_Auth/app-with-sequelize/src/services/user.service.js
+++ m3-back_end/bloco23-ORM_Auth/app-with-sequelize/src/services/user.service.js
@@ -6,6 +6,48 @@ const getAll = async () => {
   return users;
 };
 
+const getById = async (id) => {
+  const user = await User.findByPk(id);
+
+  return user;
+};
+
+const getByIdAndEmail = async (id, email) => {
+  const user = await User.findOne({ where: { id, email }});
+
+  return user;
+};
+
+const createUser = async (fullName, email) => {
+  const newUser = await User.create({ fullName, email });
+
+  return newUser;
+};
+
+const updateUser = async (id, fullName, email) => {
+  const [updatedUser] = await User.update(
+    { fullName, email },
+    { where: { id } },
+  );
+
+  console.log(updatedUser);
+  return updatedUser;
+};
+
+const deleteUser = async (id) => {
+  const user = await User.destroy(
+    { where: { id } },
+  );
+
+  console.log(user);
+  return user;
+};
+
 module.exports = {
   getAll,
+  getById,
+  getByIdAndEmail,
+  createUser,
+  updateUser,
+  deleteUser,
 };
