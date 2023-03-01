ef1d6d67806f9b8e5879ae4d7eea38e23ac3805e
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-03-01 08:40:34 -0300

    transactions

--- m3-back_end/bloco23-ORM_Auth/exemplo-rel1-1/src/controllers/employee.controller.js
+++ m3-back_end/bloco23-ORM_Auth/exemplo-rel1-1/src/controllers/employee.controller.js
@@ -32,4 +32,16 @@ const getById = async (req, res) => {
   }
 };
 
-module.exports = { getAll, getById };
+const insert = async (req, res) => {
+  try {
+    const { firstName, lastName, age, city, street, number } = req.body;
+    const employee = await EmployeeService.insert({ firstName, lastName, age, city, street, number });
+
+    return res.status(201).json({ id: employee.id, message: 'Cadastrado com sucesso' });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ message: 'Ocorreu um erro' });
+  }
+};
+
+module.exports = { getAll, getById, insert };
--- m3-back_end/bloco23-ORM_Auth/exemplo-rel1-1/src/services/employee.service.js
+++ m3-back_end/bloco23-ORM_Auth/exemplo-rel1-1/src/services/employee.service.js
@@ -1,4 +1,14 @@
+/**
+* @param {import('sequelize').Sequelize} sequelize
+*/
+
 const { Address, Employee } = require('../models');
+const Sequelize = require('sequelize');
+const config = require('../config/config');
+
+const env = process.env.NODE_ENV || 'development';
+// Ajustamos para usar a configuracao correta para nosso ambiente
+const sequelize = new Sequelize(config[env]);
 
 const getAll = async () => {
   const users = await Employee.findAll({
@@ -18,7 +28,34 @@ const getById = async (id) => {
   return employee;
 };
 
+const insert = async ({ firstName, lastName, age, city, street, number }) => {
+  const t = await sequelize.transaction();
+  try {
+    const employee = await Employee.create(
+      { firstName, lastName, age },
+      { transaction: t },
+    );
+
+    await Address.create(
+      { city, street, number, employeeId: employee.id },
+      { transaction: t },
+    );
+
+    // Se chegou ate essa linha, quer dizer que nenhum erro ocorreu
+    // Com isso, podemos finalizar a transacao usando a funcao commit
+    await t.commit();
+    return employee;
+  } catch (e) {
+    // Se entrou nesse bloco e porque alguma operacao falhou
+    // Nesse caso, o sequelize ira reverter as operacoes anteriores com a funcao rollback
+    await t.rollback();
+    console.log(e);
+    throw e; // Jogamos o erro para o controller tratar
+  }
+};
+
 module.exports = {
   getAll,
   getById,
+  insert,
 };
