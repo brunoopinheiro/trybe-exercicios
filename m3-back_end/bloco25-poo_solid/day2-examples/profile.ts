81369c3408c41c1ac2ef90532e33d5e67086eb0d
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-03-20 10:58:10 -0300

    composicao

---
+++ m3-back_end/bloco25-poo_solid/day2-examples/profile.ts
@@ -0,0 +1,38 @@
+class Profile {
+  private name: string;
+  private email: string;
+
+  constructor() {
+    this.name = '';
+    this.email = '';
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  public setEmail(email:string) {
+    this.email = email;
+  }
+
+  public toString(): string {
+    return `name - [${this.name}] - email - [${this.email}]`;
+  }
+}
+
+class SocialMediaAccount {
+  private profile = new Profile();
+
+  public editProfile(name: string, email: string) {
+    this.profile.setName(name);
+    this.profile.setEmail(email);
+  }
+
+  public printProfile() {
+    console.log(this.profile.toString());
+  }
+}
+
+const socialMediaAccount = new SocialMediaAccount();
+socialMediaAccount.editProfile('Manuella', 'manu@trybe.com');
+socialMediaAccount.printProfile();
