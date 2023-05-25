0f8b7aa2716353fc4a4b9e390e1dfab0eb7a4e13
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-25 10:43:44 -0300

    study: first python scripts

---
+++ m4-computer_science/python-basics/people.py
@@ -0,0 +1,14 @@
+import area
+
+
+PEOPLE_PER_SQUARE_METER = 2
+FIELD_LENGTH = 60
+FIELD_WIDTH = 45
+people_at_concert = (
+    area.rectangle(
+        FIELD_LENGTH,
+        FIELD_WIDTH,
+    )
+    * PEOPLE_PER_SQUARE_METER
+)
+print("Estão presentes no show aproximadamente", people_at_concert, "pessoas.")
