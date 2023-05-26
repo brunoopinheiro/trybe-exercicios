b88055865735a031b75c3b22acfea8cc5cf09a08
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-26 11:56:59 -0300

    testing in python

---
+++ m4-computer_science/python-io/codigo.py
@@ -0,0 +1,8 @@
+def is_odd(number):
+    """Retorna True se um número é impar, senão False"""
+    return number % 2 != 0
+
+
+def divide(a, b):
+    """Retorna a divisão de a por b"""
+    return a / b
