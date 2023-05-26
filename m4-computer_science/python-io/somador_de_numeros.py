c650d99769a8328c956391152ad6d3acd7c316fe
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-05-26 10:00:32 -0300

    improve i-o example

--- m4-computer_science/python-io/somador_de_numeros.py
+++ m4-computer_science/python-io/somador_de_numeros.py
@@ -1,6 +1,13 @@
-meu_numero = 0
+if __name__ == "__main__":
+    nums = input("Insira valores aqui, separados por espaço: ")
 
-while meu_numero < 42:
-    meu_numero += int(input("Digite seu número: "))
+    nums_arr = nums.split(" ")
 
-print("A soma de seus numeros superou 42!")
+    sum = 0
+    for num in nums_arr:
+        if not num.isdigit():
+            print(f"Erro ao somar valores, {num} não é um dígito.")
+        else:
+            sum += int(num)
+
+    print(f"A soma dos valores válidos é: {sum}")
