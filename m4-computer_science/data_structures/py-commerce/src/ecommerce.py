8c77990e3c1b38ef4d108f9c616bebf13bc2f01a
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-06-05 17:44:37 -0300

    feat: partially implements PremiumCustomer

--- m4-computer_science/data_structures/py-commerce/src/ecommerce.py
+++ m4-computer_science/data_structures/py-commerce/src/ecommerce.py
@@ -31,8 +31,37 @@ class Customer:
         )
 
 
+class PremiumCustomer(Customer):
+    def __init__(
+        self, name: str, email: str, address: str, reward_points: int = 0
+    ) -> None:
+        super().__init__(name, email, address)
+        self.reward_points = reward_points
+
+    def add_reward_points(self, points: int):
+        self.reward_points += points
+
+    def show_details(self):
+        super().show_details()
+        print(f"Pontos: {self.reward_points}")
+
+
 @dataclass
 class Product:
     name: str
     unit_price: float
     stock_quantity: int = 0
+
+
+class ShoppingCart:
+    def __init__(self) -> None:
+        self._items: list[tuple[Product, int]] = []
+
+    def add_item(self, product_quantity: tuple[Product, int]):
+        self._items.append(product_quantity)
+
+    def items_quantity(self):
+        return sum(item[1] for item in self._items)
+
+    def total_price(self):
+        return sum(p.unit_price * q for p, q in self._items)
