2abda0a67d8aab84032270119d8900127bf534f3
Bruno Pinheiro <bruno.opinheiro@outlook.com>
2023-06-06 10:14:19 -0300

    feat: resolucao log exercicio

---
+++ m4-computer_science/data_structures/log-poo/src/log.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+
+class ManipuladorDeLog(ABC):
+    @classmethod
+    @abstractmethod
+    def log(cls, msg):
+        raise NotImplementedError
+
+
+class LogEmArquivo(ManipuladorDeLog):
+    @classmethod
+    def log(cls, msg):
+        with open("log.txt", "a") as arquivo:
+            print(msg, file=arquivo)
+
+
+class LogEmTela(ManipuladorDeLog):
+    @classmethod
+    def log(cls, msg):
+        print(msg)
+
+
+class Log:
+    def __init__(self, manipuladores) -> None:
+        self.__manipuladores = set(manipuladores)
+
+    def adicionar_manipulador(self, manipulador):
+        self.__manipuladores.add(manipulador)
+
+    def info(self, msg):
+        self.__log("INFO", msg)
+
+    def alerta(self, msg):
+        self.__log("ALERTA", msg)
+
+    def erro(self, msg):
+        self.__log("ERRO", msg)
+
+    def debug(self, msg):
+        self.__log("DEBUG", msg)
+
+    def __log(self, nivel, msg):
+        for manipulador in self.__manipuladores:
+            manipulador.log(self.__formatar(nivel, msg))
+
+    def __formatar(self, nivel, msg):
+        data = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        formatedmsg = f"[{nivel} - {data}]: {msg}"
+        return formatedmsg
+
+
+if __name__ == "__main__":
+    log_em_tela = LogEmTela()
+    log = Log([log_em_tela])
+    log.erro("ZeroDivisionError: division by zero")
